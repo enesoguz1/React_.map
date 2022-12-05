60f44c16e366f4fbe5aba064e2a3bad5f487bad2
Enes Oğuz <enesoguz10@gmail.com>
2022-12-05 11:09:50 +0300

    .map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render() {
+    this.state = [
+      {name: "Ahmet", age: "51", school: "Oxford"},
+      {name: "Mehmet", age: "20", school: "İTÜ"},
+      {name: "Enes", age: "26", school: "ODTÜ"},
+    ];
+    return (
+      <div>
+        {this.state.map((userInfo) => {
+          return(
+            <div>
+              <h1>{userInfo.name}</h1>
+              <h2>{userInfo.age}</h2>
+              <h3>{userInfo.school}</h3>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
